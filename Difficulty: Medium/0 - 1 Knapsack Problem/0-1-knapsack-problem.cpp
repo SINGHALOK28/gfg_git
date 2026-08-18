825664f28006d220b3894d2d91fe1cf47daba476
Alok Singh <163043435+SINGHALOK28@users.noreply.github.com>
2026-08-18 09:53:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,18 +1,20 @@
 class Solution {
   public:
   int dp[1001][1001];
-  int solve(vector<int> &wt, vector<int> &val, int W,int n){
-      if(n==0 || W==0) return 0;
-      if(dp[n][W] != -1) return dp[n][W];
-      if(wt[n-1]<=W)
-          return dp[n][W] = max( val[n-1] + solve(wt,val,W-wt[n-1],n-1) , solve(wt,val,W,n-1));
-      else if(wt[n-1]>W) return dp[n][W] = solve(wt,val,W,n-1);
+  int solve(int W, vector<int> &val, vector<int> &wt, int n){
+      if(W==0 || n==0 ) return 0;
       
+      if(dp[n][W] != -1) return dp[n][W];
+      if(wt[n-1]<=W){
+          return  dp[n][W] = max(val[n-1] + solve(W-wt[n-1],val,wt,n-1), solve(W,val,wt,n-1) );
+      }
+      else if(wt[n-1]>W)
+        return  dp[n][W] = solve(W,val,wt,n-1);
   }
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         // code here
         memset(dp,-1,sizeof(dp));
-        int n=val.size();
-        return solve(wt,val,W,n);
+    int n=val.size();    //number of items
+       return solve(W,val,wt,n);
     }
 };
